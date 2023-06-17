4e51c0baef443ee39e92cc9e45bf91bfba9ac343
mesemi <mesemigame@gmail.com>
2023-06-17 15:14:05 -0400

    t

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: 'class',
-    content: [
-        "./**/*.{html,js}",
-        'node_modules/preline/dist/*.js'
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [
-        require('preline/plugin'),
-    ],
-}
-
+	darkMode: "class",
+	content: ["./**/*.{html,js}", "node_modules/preline/dist/*.js"],
+	theme: {
+		extend: {},
+	},
+	plugins: [require("preline/plugin")],
+};

b4d24e7c236bd76130c99beb32a0071cbea84fbb
mesemi <mesemigame@gmail.com>
2024-05-26 23:22:52 -0400

    revamp

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,23 @@ module.exports = {
 	darkMode: "class",
 	content: ["./**/*.{html,js}", "node_modules/preline/dist/*.js"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'textbg': '#3D3D3D',
+				'mybg': '#0D0D0D',
+			},
+			dropShadow: {
+				glow: [
+				  "0 0px 20px rgba(255,255, 255, 0.4)",
+				  "0 0px 65px rgba(255, 255,255, 0.3)"
+				],
+				glowpink: [
+					"0 0px 20px rgba(254, 205, 211, .4)",
+					"0 0px 65px rgba(254, 205, 211, .3)"
+				]
+			  }
+		},
+		
 	},
 	plugins: [require("preline/plugin")],
 };
